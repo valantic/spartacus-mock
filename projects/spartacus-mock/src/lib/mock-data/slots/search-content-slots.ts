dfd4141b4c089dec7f6b4d745ca51613a2ee3bd0
Jan Widmer <jan.widmer@cec.valantic.com>
2023-04-24 14:05:33 +0200

    #8 remove empty slots

--- projects/spartacus-mock/src/lib/mock-data/slots/search-content-slots.ts
+++ projects/spartacus-mock/src/lib/mock-data/slots/search-content-slots.ts
@@ -5,8 +5,6 @@ import { flexTypeComponent } from '../components/default/flex-type-component';
 export const cmsSearchContentSlots = (listSlotName: string, listFlexTypeCode: string): ContentSlot[] => {
   // content slots
   return [
-    contentSlot('Section2', []),
-    contentSlot('Section4', []),
     contentSlot(listSlotName, [flexTypeComponent(listFlexTypeCode)]),
     contentSlot('ProductLeftRefinements', [flexTypeComponent('ProductRefinementComponent')]),
   ];
