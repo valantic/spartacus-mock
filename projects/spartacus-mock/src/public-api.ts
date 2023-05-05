185b7da2c01f4088e11f4188dceeeb4576372da1
Jan Widmer <jan.widmer@cec.valantic.com>
2023-05-05 11:42:38 +0200

    #7 add docs, remove obsolete exports

--- projects/spartacus-mock/src/public-api.ts
+++ projects/spartacus-mock/src/public-api.ts
@@ -4,7 +4,5 @@
 export * from './lib/index';
 export * from './lib/types';
 export * from './lib/defaultRoutes';
-export * from './lib/defaultPassthrough';
 export * from './lib/local-storage';
-export * from './lib/handlers';
 export * from './lib/mock-data';
